dd885bffdee6557389fec77820d8c55b4f239df4
mohammed <mohammedhn385@gmail.com>
2026-01-20 12:39:25 +0300

    Add variable 'c' and update output conditions

--- basic/condition/number(abc)_if a+c=b....cpp
+++ basic/condition/number(abc)_if a+c=b....cpp
@@ -5,14 +5,15 @@ int main()
 {
 	while (true)
 	{
-		int n,a,b;
+		int n,a,b,c;
 		cin >> n;
 		a = n / 100;
 		b = n % 10;
+		c=(n%100/10)
 		if (a == b)
-			cout << "a=b"<<endl;
+			cout << "a+b=c"<<endl;
 		else
-			cout << "a!=b"<<endl;
+			cout << "a+b!=c"<<endl;
 	}
 		return 0;
 	
