2a4d18d6c76be52000a5c27a6ad01922ac6202a1
mohammed <mohammedhn385@gmail.com>
2026-01-20 12:42:58 +0300

    Fix variable assignments and output formatting

--- basic/condition/number(abc)_if a+c=b....cpp
+++ basic/condition/number(abc)_if a+c=b....cpp
@@ -5,16 +5,16 @@ int main()
 {
 	while (true)
 	{
-		int n,a,b,c;
+		int n, a, b, c;
 		cin >> n;
 		a = n / 100;
-		b = n % 10;
-		c=(n%100/10)
-		if (a == b)
-			cout << "a+b=c"<<endl;
-		else
-			cout << "a+b!=c"<<endl;
+		c = n % 10;
+		b = (n % 100) / 10;
+			if (a+c == b)
+				cout << "a+c=b" << endl;
+			else
+				cout << "a+c!=b" << endl;
 	}
-		return 0;
-	
+	return 0;
+
 }
